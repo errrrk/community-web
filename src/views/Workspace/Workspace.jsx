424ec1d247db72ccd396f025c4ef098bf3e17d8a
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-09-27 15:25:22 +0200

    Remove unused imports to fix linter

--- src/views/Workspace/Workspace.jsx
+++ src/views/Workspace/Workspace.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // react components for routing our app without refresh
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 // @material-ui/icons
@@ -14,7 +14,7 @@ import GridItem from "components/Grid/GridItem.jsx";
 import Parallax from "components/Parallax/Parallax.jsx";
 // sections for this page
 import HeaderLinks from "components/Header/HeaderLinks.jsx";
-import SectionBasics from "./Sections/SectionBasics.jsx";
+// import SectionBasics from "./Sections/SectionBasics.jsx";
 
 import componentsStyle from "assets/jss/material-kit-react/views/components.jsx";
 import SectionPills from "./Sections/SectionPills.jsx";
