f25a95a331dae1e0d29e0ce0e7497cfcd3fd755d
Errrrk <43416374+errrrk@users.noreply.github.com>
2018-09-27 15:07:04 +0200

    Remove .env/ from .gitignore to provide base path for node build process

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
-import indexRoutes from "./routes/index.jsx";
+import indexRoutes from "routes/index.jsx";
 
-import "./assets/scss/material-kit-react.css?v=1.2.0";
+import "assets/scss/material-kit-react.css?v=1.2.0";
 
 var hist = createBrowserHistory();
 
